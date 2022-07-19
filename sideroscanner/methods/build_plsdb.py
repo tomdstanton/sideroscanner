f1dffc0dccf8f38e3d280c77d46c5d747ab40122
HassanAlmana <52850375+HassanAlmana@users.noreply.github.com>
2022-07-19 09:54:02 +0100

    add logic for tarball files in plasdb

--- sideroscanner/methods/build_plsdb.py
+++ sideroscanner/methods/build_plsdb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import tarfile
 from pathlib import Path
 from zipfile import ZipFile
 from os import remove
@@ -11,8 +12,12 @@ def get_plsdb(plspath):
     print("[-] Preparing plasmid database")
     Path(plspath).mkdir(parents=True, exist_ok=True)
     fetch_url('https://ccb-microbe.cs.uni-saarland.de/plsdb/plasmids/download/?zip', None, plspath + '/plsdb')
-    with ZipFile(f'{plspath}/plsdb', "r") as zip_ref:
-        zip_ref.extractall(f'{plspath}/')
+    if tarfile.is_tarfile(f'{plspath}/plsdb'):
+        with tarfile.open(f'{plspath}/plsdb', "r") as tar_ref:
+            tar_ref.extractall(f'{plspath}/')
+    else:
+        with ZipFile(f'{plspath}/plsdb', "r") as zip_ref:
+            zip_ref.extractall(f'{plspath}/')
     for f in ['/plsdb.tsv','/plsdb_changes.tsv','/plsdb.msh','/plsdb.abr','/plsdb.sim','/README.md']:
         remove(plspath + f)
     return
